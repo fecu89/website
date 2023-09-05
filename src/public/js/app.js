e4bcece04d6089570ef19bf078ef5a77a1e4dfcc
fecu89 <fecu@kakao.com>
2023-09-05 14:02:48 +0900

    20230905 commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
-alert("hi!");
+// alert("hi!");
+
+// const socket = new WebSocket("http://localhost:3000")
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log(`Just got this ${message.data} frome the server`);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Dissconnected from Server");
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -12,4 +14,23 @@ app.get("/*", (req,res) => res.redirect("/"))
 
 
 const handleListen = () => console.log("Listening on http://localhost:3000")
-app.listen(3000, handleListen)
+// app.listen(3000, handleListen)
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+// 에로우 펑션을 변경하면 아래와 같음
+// 1. 원래 함수
+// function handleConnection(socket){
+//     console.log(socket)
+// }
+// wss.on("connection", handleConnection)
+
+// 2. 에로우 펑션을 활용한 함수
+wss.on("connection", (socket) => {
+    // console.log(socket);
+    console.log("Connected to Browser")
+    socket.send("hello!")
+});
+
+server.listen(3000, handleListen)
