71df9441201fcf5964f94bce9c7827f000f98708
fecu89 <fecu@kakao.com>
2023-09-22 14:43:43 +0900

    painter

---
+++ paint/app.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.rect(50, 50, 100, 200);
+ctx.rect(150, 250, 100, 200);
+ctx.stroke();
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.beginPath();
+ctx.rect(250, 50, 100, 200);
+ctx.stroke();
