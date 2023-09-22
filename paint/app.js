e5f785491550ee9b3c90338dfadc98d34c144594
fecu89 <fecu@kakao.com>
2023-09-22 18:15:27 +0900

    mouse up & down event

--- paint/app.js
+++ paint/app.js
@@ -1,13 +1,30 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 300;
+canvas.height = 300;
 
 ctx.lineWidth = 2;
+let isPainting = false;
 
-function onClick(event){
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
     };
+    ctx.moveTo(event.offsetX, event.offsetY);
+};
 
-canvas.addEventListener("click", onClick);
+function startPainting(event) {
+    console.log("mouse down");
+    isPainting = true;
+};
+function cancelPainting(event) {
+    console.log("mouse up");
+    isPainting = false;
+};
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
