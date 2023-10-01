a38b934b080a5fe95598792145802346d7e0ced1
fecu89 <fecu@kakao.com>
2023-10-01 18:29:30 +0900

    range input

--- paint/app.js
+++ paint/app.js
@@ -1,9 +1,10 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 300;
 canvas.height = 300;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event) {
@@ -22,9 +23,15 @@ function startPainting(event) {
 function cancelPainting(event) {
     console.log("mouse up");
     isPainting = false;
+    ctx.beginPath();
 };
+function onLineWidthChgange(event){
+    ctx.lineWidth = event.target.value;
+};
+
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+lineWidth.addEventListener("change", onLineWidthChgange);
